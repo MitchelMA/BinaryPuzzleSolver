4cbe388e99da1820990a7fe2305c2e1640ee8d58
Mitchel Vonk <vonkm124@gmail.com>
2025-03-23 18:25:59 +0100

    Verdere verbeteringen aan de line-count strategy

--- BinaryPuzzleSolver/Strategies/LineCountStrategy.cs
+++ BinaryPuzzleSolver/Strategies/LineCountStrategy.cs
@@ -7,18 +7,19 @@ public class LineCountStrategy : LineStrategy
     protected override bool ProcessHorizontal(int yIndex, FieldValues[][] field)
     {
         var horizontal = field[yIndex];
-        var openCount = horizontal.Count(x => x == FieldValues.Open);
-        if (openCount is > 2 or 0)
-            return false;
-
         var width = horizontal.Length;
+        var halfWidth = width / 2;
         var oneCount = horizontal.Count(x => x == FieldValues.One);
-        var zeroCount = width - oneCount - openCount;
-        var diff = oneCount - zeroCount;
-        if (diff == 0)
+        var zeroCount = horizontal.Count(x => x == FieldValues.Zero);
+        var openCount = width - oneCount - zeroCount;
+
+        if (openCount == 0 || openCount > halfWidth)
             return false;
 
-        var underRepresented = diff > 0 ? FieldValues.Zero : FieldValues.One;
+        if (oneCount != halfWidth && zeroCount != halfWidth)
+            return false;
+
+        var underRepresented = oneCount > zeroCount ? FieldValues.Zero : FieldValues.One;
         for (int i = 0; i < width; i++)
             if (horizontal[i] == FieldValues.Open)
                 horizontal[i] = underRepresented;
@@ -29,19 +30,19 @@ public class LineCountStrategy : LineStrategy
     protected override bool ProcessVertical(int xIndex, FieldValues[][] field)
     {
         var vertical = field.Select(x => x[xIndex]).ToArray();
-        var openCount = vertical.Count(x => x == FieldValues.Open);
-        if (openCount is > 2 or 0)
-            return false;
-
         var height = vertical.Length;
+        var halfHeight = height / 2;
         var oneCount = vertical.Count(x => x == FieldValues.One);
-        var zeroCount = height - oneCount - openCount;
-        var diff = oneCount - zeroCount;
+        var zeroCount = vertical.Count(x => x == FieldValues.Zero);
+        var openCount = height - oneCount - zeroCount;
 
-        if (diff == 0)
+        if (openCount == 0 || openCount > halfHeight)
             return false;
 
-        var underRepresented = diff > 0 ? FieldValues.Zero : FieldValues.One;
+        if (oneCount != halfHeight && zeroCount != halfHeight)
+            return false;
+
+        var underRepresented = oneCount > zeroCount ? FieldValues.Zero : FieldValues.One;
         for (int i = 0; i < height; i++)
             if (vertical[i] == FieldValues.Open)
                 field[i][xIndex] = underRepresented;
